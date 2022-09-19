9a79f7f5fbc5ed1cf3e412a527fb0f4bc050ad81
hung <hungptit2306@gmail.com>
2022-09-19 10:58:56 +0700

    update

--- src/main/java/com/gitlab/gitlab/controller/Test.java
+++ src/main/java/com/gitlab/gitlab/controller/Test.java
@@ -1,7 +1,13 @@
 package com.gitlab.gitlab.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Test {
+
+    @GetMapping("/hung")
+    public String haha() {
+        return "hung";
+    }
 }
